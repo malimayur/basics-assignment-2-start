7382aca629ea2a9683d870ad402e4661bcc3fc9e
Mayur Mali <mayur.mali@gmail.com>
2017-08-27 19:02:49 +0530

    intial commit

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  userName = '';
+  allowToAddUser = true;
+  constructor() {
+  }
+  ngOnInit() {
+  }
+
+  isAllowAddUserName() {
+    if (this.userName !== '') {
+      this.allowToAddUser = false;
+    }
+  }
+}
